8aed011c1f4f15f8043c11342bf930fe0ecb9227
Dulio Denis <dulio.denis@yahoo.com>
2019-01-11 16:31:06 -0500

    Day 42. Course 9: Finished Exceptions

---
+++ unit_04/09-Testing/2-Be_Assertive/5_tests.py
@@ -0,0 +1,53 @@
+#
+#  Python Testing: Be Assertive 
+#  Python Techdegree
+#
+#  Created by Dulio Denis on 1/11/19.
+#  Copyright (c) 2019 ddApps. All rights reserved.
+# ------------------------------------------------
+#  The `unittest` library has many different
+#  assertions but some are more commonly used than
+#  others. Let's look at how to check for membership,
+#  what class a variable is an instance of, and a
+#  couple of others.
+# ------------------------------------------------
+import unittest
+
+from dice import Die, Roll
+
+class DieTest(unittest.TestCase):
+    def setUp(self):
+        self.d6 = Die(6)
+        self.d8 = Die(8)
+
+    def test_creation(self):
+        self.assertEqual(self.d6.sides, 6)
+        self.assertIn(self.d6.value, range(1,7))
+
+    def test_add(self):
+        self.assertIsInstance(self.d6+self.d8, int)
+
+class RollTest(unittest.TestCase):
+    def setUp(self):
+        self.hand1 = Roll('1d2')
+        self.hand3 = Roll('3d6')
+
+    # test upper and lower boundary on these rolls
+    def test_lower(self):
+        self.assertGreaterEqual(int(self.hand3), 3)
+
+    def test_upper(self):
+        self.assertLessEqual(int(self.hand3), 18)
+
+    def test_membership(self):
+        test_die = Die(2)
+        test_die.value = self.hand1.results[0].value
+        self.assertIn(test_die, self.hand1.results)
+
+    def test_bad_description(self):
+        with self.assertRaises(ValueError):
+            Roll('2b6')
+
+    def test_small_die(self):
+        with self.assertRaises(ValueError):
+            Roll('1d2') # totally acceptable - won't fail
